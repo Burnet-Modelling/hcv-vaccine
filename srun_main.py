fcb5533bb9ce85ae4381c147d9e27d9c717e096b
farahhoudroge <farah.houdroge@outlook.com>
2026-02-16 17:01:46 +1100

    recal pop

--- srun_main.py
+++ srun_main.py
@@ -36,11 +36,43 @@ savedir_scens.mkdir(parents=True, exist_ok=True)
 sens_folder = results_folder / 'sensitivity_analyses'
 sens_folder.mkdir(parents=True, exist_ok=True)
 
+
+#%%
+P = ut.project(country, load_calibration=True, cal_folder=cal_folder, load_programs=False,cal_version='v1')
+_, D = ut.return_fw_db(country)
+parset = P.make_parset()
+parset.load_calibration(cal_folder/f'{country}_calibration.xlsx')   
+result = P.run_sim(parset=parset) 
+ut_plt.plot_outcomes2(P, result, country, start_year=2000, end_year=2050)
+
+cal = P.parsets.copy('default','Calibrated')
+
+par_adj = ['18-64_males','PWID_males']
+pop_adj = ['Prisoners_males','Prisoners_PWID_males']
+# measurable
+par_meas = ['alive','alive']
+pop_meas = ['Prisoners_males','Prisoners_PWID_males']
+bounds = [(0, None),(0, None)]
+
+ut.optimise_y_factor(country, cal_folder, parset, P, D, par_adj, pop_adj, par_meas, pop_meas, bounds,transfer='inc') 
+P = ut.project(country, load_calibration=True, cal_folder=cal_folder, load_programs=False)
+cal = P.parsets.copy('default','Calibrated')
+
+par_adj = ['18-64_males']
+pop_adj = ['PWID_males']
+# measurable
+par_meas = ['alive']
+pop_meas = ['PWID_males']
+bounds = [(0, None)]
+
+ut.optimise_y_factor(country, cal_folder, parset, P, D, par_adj, pop_adj, par_meas, pop_meas, bounds,transfer='idu_status') 
+P = ut.project(country, load_calibration=True, cal_folder=cal_folder, load_programs=False)
+#%%
 # Generate databook
-generate_databook(country)
+# generate_databook(country)
 #%%
 # Run calibration
-ut.run_calibration(country, savedir=cal_folder)
+# ut.run_calibration(country, savedir=cal_folder)
 ut_plt.plot_calibration(country, cal_folder=cal_folder, savedir=savedir_calib, cal_version="v1") # plot
 
 # Run vaccine scenarios
