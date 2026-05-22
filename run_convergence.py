2bb479e00bf2424fe5d84b1dd45dbdec3538546b
Farah Houdroge <64887646+farahhoudroge@users.noreply.github.com>
2026-05-22 14:40:00 +0200

    int samples

--- run_convergence.py
+++ run_convergence.py
@@ -9,20 +9,20 @@ Script to run convergence analysis on sample size (number of simulations).
 from hcv.generate_databooks import generate_databook
 from hcv import utils as ut
 from hcv import utils_plotting as ut_plt
-import os
 import sys
 
 dir = ut.get_project_root()
 
 country = "VNM"
 rand_seed = 250711
-n_samples = int(sys.argv[1])
+n_samples = sys.argv[1]
 results_folder = dir / "results"
 cal_folder = results_folder / "calibration" / "y-factors"
 savedir_scens = results_folder / "convergence" / country / n_samples
-os.makedirs(savedir_scens, exist_ok=True)
+savedir_scens.mkdir(parents=True, exist_ok=True)
 savedir_pp = results_folder / "convergence" / "results" / country / n_samples
-os.makedirs(savedir_pp, exist_ok=True)
+savedir_pp.mkdir(parents=True, exist_ok=True)
 
+n_samples=int(n_samples)
 ut.run_scenario_sampling(country, cal_folder, rand_seed=rand_seed, n_samples=n_samples, savedir=savedir_scens,nb_scenarios=1)
 ut.econ_eval(country, savedir_scens, savedir_pp, rand_seed=rand_seed, n_samples=n_samples,nb_scenarios=1)
